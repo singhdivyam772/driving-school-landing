11691b655d51f509656452b487813d5951fd9bad
Divyam Singh <singhdivyam772@gmail.com>
2025-12-25 11:11:24 +0530

    update the fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Rajesh Motor Training School",
+  description: "Rajesh Motor Training School",
+  generator: "Rajesh Motor Training School",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
